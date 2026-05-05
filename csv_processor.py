de6255b196c43662a37b2aba69a7a4459d7992d7
Mahmoud Tawfeek <mahmoudtawfeek815@gmail.com>
2026-05-06 00:39:01 +0300

    feat: add comments

--- csv_processor.py
+++ csv_processor.py
@@ -4,18 +4,18 @@ import csv
 def main():
     filename = 'data.csv'
     try:
-        with open(filename, mode='r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
+        with open(filename, mode='r', encoding='utf-8') as file:   #mode=r --> to close the file after reading
+            reader = csv.DictReader(file)  #to eery row to dict
             
             try:
                 first_row = next(reader)
                 print("First row found:")
                 print(first_row)
+                # if file is empty
             except StopIteration:
                 print("The CSV file is empty.")
-                
+                #if file not found
     except FileNotFoundError:
         print(f"Error: {filename} not found. Please create a data.csv file.")
 
-if __name__ == "__main__":
     main()
