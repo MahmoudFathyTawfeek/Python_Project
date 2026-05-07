fb95bb631f2e2d470ffa947cbdb465c2f41a72a5
Mahmoud Tawfeek <mahmoudtawfeek815@gmail.com>
2026-05-07 12:19:14 +0300

    fix: update the condition to run the file

--- csv_processor.py
+++ csv_processor.py
@@ -5,7 +5,7 @@ def main():
     filename = 'data.csv'
     try:
         with open(filename, mode='r', encoding='utf-8') as file:   #mode=r --> to close the file after reading
-            reader = csv.DictReader(file)  #to eery row to dict
+            reader = csv.DictReader(file)  #every row to dict
             
             try:
                 first_row = next(reader)
@@ -14,8 +14,8 @@ def main():
                 # if file is empty
             except StopIteration:
                 print("The CSV file is empty.")
-                #if file not found
+      #if file not found          
     except FileNotFoundError:
         print(f"Error: {filename} not found. Please create a data.csv file.")
-
+# if __name__ == "__main__":    #تشغيل الكود عند تشغيل الملف وليس عند استدعاءه
     main()
